d60b650d1568021831d3abeafb254795dac25323
Ramanuj Sarkar <ramanuj.sarkar.ge@gmail.com>
2025-06-02 16:37:28 -0700

    Added memo file and code to read it
    This code reads the .m4a file and prints out what it says.

--- app/utils/voice_processing.py
+++ app/utils/voice_processing.py
@@ -1,3 +1,11 @@
 # Functions for transcribing voice with Whisper
 
 import whisper
+import certifi
+
+model = whisper.load_model("base")
+result = model.transcribe("VoiceMemoFile.m4a")
+voice_command = result["text"]
+
+if __name__ == '__main__':
+    print(voice_command)
