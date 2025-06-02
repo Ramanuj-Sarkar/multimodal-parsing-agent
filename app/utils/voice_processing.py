47d8b511cb25bc8f6bef5920bd7d5204f8e643bd
Ramanuj Sarkar <ramanuj.sarkar.ge@gmail.com>
2025-06-02 16:43:34 -0700

    Took out certifi
    It didn't need certifi.

--- app/utils/voice_processing.py
+++ app/utils/voice_processing.py
@@ -1,10 +1,9 @@
 # Functions for transcribing voice with Whisper
 
 import whisper
-import certifi
 
 model = whisper.load_model("base")
-result = model.transcribe("VoiceMemoFile.m4a")
+result = model.transcribe("voice_memo.m4a")
 voice_command = result["text"]
 
 if __name__ == '__main__':
