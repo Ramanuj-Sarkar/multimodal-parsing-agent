ffa7780419454a2d5e5a23b494e1d6695a7982d1
Ramanuj Sarkar <ramanuj.sarkar.ge@gmail.com>
2025-06-02 15:42:24 -0700

    Update image_processing.py
    The image processing should work now, even with more images.

--- app/utils/image_processing.py
+++ app/utils/image_processing.py
@@ -1,4 +1,17 @@
 # Functions for image captioning, OCR, etc.
 
 import pytesseract
-import openai
+from PIL import Image
+
+# This might need to be changed at some point.
+# If tesseract isn't found or something
+# look at this:
+# https://stackoverflow.com/questions/50951955/pytesseract-tesseractnotfound-error-tesseract-is-not-installed-or-its-not-i
+pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract'
+
+# get image and stuff
+image = Image.open("doritos_label.png")
+text = pytesseract.image_to_string(image)
+
+if __name__ == '__main__':
+    print(text)
